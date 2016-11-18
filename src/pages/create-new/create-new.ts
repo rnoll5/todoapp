3b6e4cec110b9fb53a069f4250855e4d74d8e8c8
jbrownssf <jpbrown@softstackfactory.org>
2016-11-18 19:59:14 +0000

    new list

--- src/pages/create-new/create-new.ts
+++ src/pages/create-new/create-new.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+import { MyListsPage } from '../my-lists/my-lists';
+import { SharedListsPage } from '../shared-lists/shared-lists';
 
 /*
   Generated class for the CreateNew page.
@@ -13,10 +17,68 @@ import { NavController } from 'ionic-angular';
 })
 export class CreateNewPage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController) {}
 
   ionViewDidLoad() {
     console.log('Hello CreateNewPage Page');
   }
-
+    public todoList = [];
+    // public todoItems = [];
+    todos = {};
+ 
+    onPageDidEnter() {
+        this.todoList = JSON.parse(localStorage.getItem("todos"));
+        if(!this.todoList) {
+            this.todoList = [];
+        }
+        // this.todoItem = "";
+    }
+ 
+    delete(index: number) {
+        this.todoList.splice(index, 1);
+        localStorage.setItem("todos", JSON.stringify(this.todoList));
+    }
+    
+    add() {
+        // if(this.todoItem != "") {
+            this.todoList.push("");
+            localStorage.setItem("todos", JSON.stringify(this.todoList));
+            // this.navCtrl.pop();
+        // }
+    }
+ 
+    save() {
+        this.navCtrl.push(MyListsPage);
+    }
+    
+    saveShare() {
+    let prompt = this.alertCtrl.create({
+      title: 'Share my list with a friend',
+      message: "Enter the registered email of your friend",
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+    this.navCtrl.push(SharedListsPage);
+  }
+  
 }
